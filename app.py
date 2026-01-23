9e58a11fecfe99bef50989a01f685de5fa2e0ce3
Leslie Michelle Vazquez Gutierrez <Leslie>
2026-01-22 21:11:24 -0600

    macros

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    titulo="IDGS803-Flask"
+    lista=['Pedro','mario','juan']
+    return render_template('index.html',titulo=titulo, lista=lista)
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template('alumnos.html')
+
+@app.route("/usuario")
+def usuarios():
+    return render_template('usuario.html')
 
 @app.route('/hola')
 def hola():
@@ -42,6 +52,63 @@ def operas():
         <input type="text" id="name" name="name" required>
     </form>
             '''
+
+@app.route("/operaBas",methods=["GET", "POST"])
+def operas1():
+
+    resultado=0
+    if request.method == 'POST':
+        n1 = float(request.form.get("n1"))
+        n2 = float(request.form.get("n2"))
+    operacion = request.form.get("operacion")
+
+    if operacion == "suma":
+        resultado = n1 + n2
+        op = "suma"
+
+    elif operacion == "resta":
+        resultado = n1 - n2
+        op = "resta"
+
+    elif operacion == "multiplicacion":
+        resultado = n1 * n2
+        op = "multiplicación"
+
+    elif operacion == "division":
+        if n2 == 0:
+            return "<h1>Entre 0 no</h1>"
+        resultado = n1 / n2
+        op = "división"
+
+        
+    return render_template('operaBas.html',resultado=resultado)
+
+@app.route("/resultado", methods=["GET", "POST"])
+def resultado():
+
+    n1 = float(request.form.get("n1"))
+    n2 = float(request.form.get("n2"))
+    operacion = request.form.get("operacion")
+
+    if operacion == "suma":
+        resultado = n1 + n2
+        op = "suma"
+
+    elif operacion == "resta":
+        resultado = n1 - n2
+        op = "resta"
+
+    elif operacion == "multiplicacion":
+        resultado = n1 * n2
+        op = "multiplicación"
+
+    elif operacion == "division":
+        if n2 == 0:
+            return "<h1>Error: No se puede dividir entre 0</h1>"
+        resultado = n1 / n2
+        op = "división"
+
+    return f"<h1>El resultado de la {op} es: {resultado}</h1>"
 
 if __name__ == '__main__':
     app.run()
